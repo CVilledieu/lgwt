fd04a15db09c7f0a0197ef5e7f7c4351e363b858
Christopher V <ChrisRVill@gmail.com>
2024-03-04 22:42:50 -0800

    Added language options and tests

--- helloandtests/hello_test.go
+++ helloandtests/hello_test.go
@@ -4,16 +4,30 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	//Spanish translation
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	//French translation
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Christophe", "French")
+		want := "Bonjour, Christophe"
 		assertCorrectMessage(t, got, want)
 	})
 }
