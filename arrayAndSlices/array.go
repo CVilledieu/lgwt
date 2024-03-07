34745a5c946e50350bb4a6660f264f26fecc017a
Christopher V <ChrisRVill@gmail.com>
2024-03-07 01:05:59 -0800

    added SumAll func updated test to reflect change

--- arrayAndSlices/array.go
+++ arrayAndSlices/array.go
@@ -7,3 +7,14 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfnumbers := len(numbersToSum)
+	sums := make([]int, lengthOfnumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
