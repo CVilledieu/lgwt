fc90247221fcdea2bf055dcb111befba26a0cc11
Christopher V <ChrisRVill@gmail.com>
2024-03-09 13:13:53 -0800

    added circle shape and updated tests for future shapes interface

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -5,7 +5,9 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	got := Perimter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -14,10 +16,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g wanted %g", got, want)
+		}
 	}
+	t.Run("Area of a rectangle ", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("Area of a circle", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
