3fb6ac0e17a12e10877e316b68a6ec0c98e2e3ed
Christopher V <ChrisRVill@gmail.com>
2024-03-09 18:16:04 -0800

    finalized tests added error funcs and insufficients funds check

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -4,39 +4,57 @@ import (
 	"testing"
 )
 
-/*
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-
-}
-*/
-
-func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-
-	errorFunc := func(t *testing.T, got int, want int) {
-		if got != want {
-			t.Errorf("got %d wanted %d", got, want)
-		}
-	}
+	startingBalance := Bitcoin(100)
+	movingMoney := Bitcoin(10)
 
 	t.Run("deposit into wallet", func(t *testing.T) {
+		wallet := Wallet{startingBalance}
+		wallet.Deposit(movingMoney)
 
-		wallet.Deposit(10)
-		got := wallet.Balance()
-		//fmt.Printf("address in test is %p \n", &wallet.balance)
-		want := 10
-
-		errorFunc(t, got, want)
+		balanceFunc(t, wallet, startingBalance+movingMoney)
+	})
+	//withdraw amount defined as starting + deposit
+	t.Run("withdraw with sufficient funds", func(t *testing.T) {
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(movingMoney)
+		assertNoError(t, err)
+		balanceFunc(t, wallet, startingBalance-movingMoney)
 	})
 
+	t.Run("withdraw with insufficient funds", func(t *testing.T) {
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(startingBalance + movingMoney)
+
+		throwError(t, err, ErrInsufficientFunds)
+		balanceFunc(t, wallet, startingBalance)
+
+	})
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func throwError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("Didnt catch the error")
+	}
+
+	if got != want {
+		t.Errorf("got %q wanted %q", got, want)
+	}
+}
+
+func balanceFunc(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s wanted %s", got, want)
+	}
 }
